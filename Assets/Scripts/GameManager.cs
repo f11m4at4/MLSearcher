5f8619a4cff7a3da2f4e9084627408b33ec899b4
Wonseok Park <amurorei2000@gmail.com>
2021-09-13 14:18:52 +0900

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 씬에 있는 물체들을 섞어서 재배치해준다.
+// 필요속성 : 물체들, 반경, 물체간의 거리
+public class GameManager : MonoBehaviour
+{
+    // 필요속성 : 물체들, 반경, 물체간의 거리
+    public Transform[] objects;
+    public float range = 5;
+    public float distanceBetweenObj = 2;
+
+    private void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Reset();
+        }
+    }
+
+    // 씬에 있는 물체들을 섞어서 재배치해준다.
+    public void Reset()
+    {
+        // 씬에 있는 물체들을 섞어서 재배치해준다.
+        // 1. 여러개를 반복적으로
+        for (int i = 0; i < objects.Length; i++)
+        {
+            // 2. 배치할 위치가 필요
+            Vector3 pos = Random.insideUnitSphere * range;
+            pos.y = 0;
+            // 3. 재배치 해준다.
+            objects[i].localPosition = pos;
+            
+            for(int j=0;j<objects.Length;j++)
+            {
+                // 만약 나라면 건너뛴다.
+                if(i == j)
+                {
+                    continue;
+                }
+                // 만약 다른 물체들과의 거리가 일정범위를 유지 하지 않으면
+                float distance = Vector3.Distance(pos, objects[j].localPosition);
+                if (distance < distanceBetweenObj)
+                {
+                    // 다시 랜덤 위치 정하자
+                    i--;
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerAgent.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 1. 사용자의 입력에 따라 회전하고 싶다.
+// 필요속성 : 회전속도
+// 2. 사용자의 입력에 따라 (앞으로)이동하고 싶다.
+// 필요속성 : 이동속도
+public class PlayerAgent : MonoBehaviour
+{
+    // 필요속성 : 회전속도
+    public float rotSpeed = 100;
+    // 필요속성 : 이동속도
+    public float moveSpeed = 5;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 사용자의 입력에 따라 회전하고 싶다.
+        // 1. 사용자 입력에따라
+        float value = 1;
+        if(Input.GetKey(KeyCode.A))
+        {
+            value = 0;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            value = 2;
+        }
+
+        float move = 0;
+        if(Input.GetKey(KeyCode.W))
+        {
+            move = 1;
+        }
+
+        // 2. 방향이 필요
+        float dir = value - 1;
+        // 3. 회전하고 싶다.
+        transform.Rotate(Vector3.up * dir * rotSpeed * Time.fixedDeltaTime);
+        // 사용자의 입력에 따라 (앞으로)이동하고 싶다.
+        // 1. 사용자 입력에 따라
+        // 2. 방향이 필요
+        // 3. 이동하고 싶다.
+        rb.velocity = transform.forward * move * moveSpeed;
+    }
+}
